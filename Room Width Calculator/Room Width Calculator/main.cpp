65c4188aad21f576861c8983c0f7067fa4126053
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-03-31 13:47:31 +0600

    add room area calculator

---
+++ Room Width Calculator/Room Width Calculator/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  Room Width Calculator
+//
+//  Created by Farhan Niloy on 31/3/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int room_length;
+    int room_width;
+    
+    cout<<"Enter room length: " <<endl;
+    cin>>room_length;
+    
+    cout<<"Enter room width: " <<endl;
+    cin>>room_width;
+    
+    cout<<"The area of room is: " <<room_width * room_length <<" square meter" <<endl;
+    
+    return 0;
+    
+}
