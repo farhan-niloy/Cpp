08e2f4c88836c72034ed66fe8cf9691dbb27a5b7
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-04-03 13:35:04 +0600

    add conditional operator

--- Conditional Operator/Conditional Operator/main.cpp
+++ Conditional Operator/Conditional Operator/main.cpp
@@ -7,8 +7,24 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main (){
+    int num{};
+    cout<<"Enter an integer to check if it's odd or even: " ;
+    cin>>num;
+    
+    cout<<num <<" is " <<((num%2==0)?"Even":"Odd") <<endl;
+    
+    int num1{}, num2{};
+    cout<<"Enter two integers seperated by space: " ;
+    cin>>num1 >>num2;
+    
+    if(num1!=num2){
+        cout<<"Largest: " <<((num1>num2)? num1:num2) <<endl;
+        cout<<"Smallest: " <<((num2>num1)? num1:num2) <<endl;
+    } else{
+        cout<<"Numbers are the same" <<endl;
+    }
     return 0;
 }
