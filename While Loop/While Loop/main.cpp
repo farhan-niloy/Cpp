00b371a83a7e2ffb89c380cc457c4ddcbd943347
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-04-04 14:11:35 +0600

    add more code to while loop

--- While Loop/While Loop/main.cpp
+++ While Loop/While Loop/main.cpp
@@ -11,35 +11,48 @@ using namespace std;
 
 int main (){
     
-//    int num{};
-//    cout<<"Enter a positive integer - start the countdown:";
-//    cin>>num;
-//
-//    while(num>0){
-//        cout<<num<<endl;
-//        --num;
-//    }
-//
-//    int num1{};
-//    cout<<"Enter a positive integer to -start countup: ";
-//    cin>>num1;
-//
-//    int i{1};
-//    while(num1>i){
-//        cout<<i <<endl;
-//        i++;
-//
-//    }
-//
-    int num{10}, num1{20};
-    
-    int i{1};
-    
-    for(i={1}; i<=num; i++ ) {
-        cout<<i <<" \n";
-        
-        cout<<" * " <<num1;
+    int num{};
+    cout<<"Enter a positive integer - start the countdown:";
+    cin>>num;
+
+    while(num>0){
+        cout<<num<<endl;
+        --num;
     }
+
+    int num1{};
+    cout<<"Enter a positive integer to -start countup: ";
+    cin>>num1;
+
+    int i{1};
+    while(num1>i){
+        cout<<i <<endl;
+        i++;
+
+    }
+
+//    int numx{10}, numy{20};
+//
+//    int iz{1};
+//
+//    for(iz={1}; iz<=numx; i++ ) {
+//        cout<<iz <<" \n";
+//
+//        cout<<" * " <<numy;
+//    }
+    
+    int number{};
+    cout<<"Enter an integer less than 100: ";
+    cin>>number;
+    
+    while(number>=100){
+        cout<<"Enter an integer less than 100: ";
+        cin>>number;
+    }
+    
+    
+    
+    
     
     return 0;
 }
