568b329ea1c24865b984371a9770ffa7c03bdaf1
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-04-06 12:17:46 +0600

    add section 9 challenge

---
+++ Section 9 Challenge/Section 9 Challenge/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Section 9 Challenge
+//
+//  Created by Farhan Niloy on 4/4/23.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main (){
+    
+    int numbers{0};
+    
+    for(int i{1}; i<=numbers; i++){
+        
+    }
+    
+}
