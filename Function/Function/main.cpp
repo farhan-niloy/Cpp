1e7001ce0b41bf8da4800b06a02f6f040a15770f
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-06-28 13:50:51 +0600

    add function

---
+++ Function/Function/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Function
+//
+//  Created by Farhan Niloy on 28/6/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
