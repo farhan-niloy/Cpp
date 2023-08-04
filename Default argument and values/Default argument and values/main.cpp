e25699f434eed2b310d413e668da9d7d8bd8f713
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-04 14:23:46 +0600

    add function

--- Default argument and values/Default argument and values/main.cpp
+++ Default argument and values/Default argument and values/main.cpp
@@ -9,8 +9,22 @@
 #include <string>
 #include <iomanip>
 
+using namespace std;
+
+double calc_cost(double base_cost=100, double tax_rate=0.06, double shipping=3);
+
+double calc_cost(double base_cost, double tax_rate, double shipping) {
+    return base_cost += (base_cost*tax_rate)+shipping;
+};
+
 int main() {
     
+    double cost{0};
+    
+    cost = calc_cost(200, 0.08, 6);
+    
+    cout<<fixed <<setprecision(2);
+    cout<<"Cost is: " <<cost <<endl; 
     
     return 0;
 }
