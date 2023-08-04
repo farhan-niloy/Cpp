47a1fd9973010329b8c17b93cd3724c8a79f1e85
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-04 14:33:02 +0600

    add code without string concatanating

--- Default argument and values/Default argument and values/main.cpp
+++ Default argument and values/Default argument and values/main.cpp
@@ -11,20 +11,38 @@
 
 using namespace std;
 
-double calc_cost(double base_cost=100, double tax_rate=0.06, double shipping=3);
+double calc_cost(double base_cost=100, double tax_rate=0.06, double shipping=3.5);
+
+void greeting(string name, string prefix ="Mr. ", string suffix=" ");
 
 double calc_cost(double base_cost, double tax_rate, double shipping) {
     return base_cost += (base_cost*tax_rate)+shipping;
+};
+
+void greeting(string name, string prefix, string suffix){
+    cout<<"Hello, " <<prefix <<name <<suffix <<endl;
 };
 
 int main() {
     
     double cost{0};
     
+    cost = calc_cost(200, 0.08);
+    
+    cout<<fixed <<setprecision(2);
+    cout<<"Cost is: " <<cost <<endl;
+    
     cost = calc_cost(200, 0.08, 6);
     
     cout<<fixed <<setprecision(2);
-    cout<<"Cost is: " <<cost <<endl; 
+    cout<<"Cost is: " <<cost <<endl;
+    
+    cost = calc_cost(200);
+    
+    cout<<fixed <<setprecision(2);
+    cout<<"Cost is: " <<cost <<endl;
+    
+    greeting("Rust");
     
     return 0;
 }
