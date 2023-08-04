1f2be0f7e5486df48778dda2f84c681fd0db1eb1
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-04 14:10:38 +0600

    add default function argument

---
+++ Default argument and values/Default argument and values/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Default argument and values
+//
+//  Created by Farhan Niloy on 4/8/23.
+//
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+int main() {
+    
+    
+    return 0;
+}
