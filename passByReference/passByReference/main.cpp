b66bf40960fade5063c814a3ae0a2162ffef9170
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-06 15:02:11 +0600

    error fixed

--- passByReference/passByReference/main.cpp
+++ passByReference/passByReference/main.cpp
@@ -17,22 +17,22 @@ void pass_by_ref3(vector<string> &v);
 void print_vec(const vector<string> &v);
 
 void pass_by_ref1(int &n){
-    n=100;
+    n=1000;
 };
 
 void pass_by_ref2(string &s) {
     s = "Many Many Dollars Needed";
 }
 
-void pass_by_ref3(vector<string>v){
+void pass_by_ref3(vector<string> &v){
     v.clear();
-};
+}
 
-void print_vec(const vector<string> v) {
+void print_vec(const vector<string> &v) {
     for (auto s:v)
         cout<<s<<" ";
     cout<<endl;
-};
+}
 
 int main(){
     int n{100};
@@ -40,22 +40,27 @@ int main(){
     vector<string> stooges {"larry", "curly", "moe"};
     
     cout<<"without calling pass_by_ref1: " <<n <<endl;;
-    cout<<"\n calling using pass_by_ref1: ";
     pass_by_ref1(n);
+    cout<<"calling using pass_by_ref1: " <<n;
+    cout<<endl <<endl;
     
     
     cout<<"without calling pass_by_ref2: " <<s <<endl;
-    cout<<"calling with pass_by_ref2: " <<endl;
     pass_by_ref2(s);
+    cout<<"calling with pass_by_ref2: " <<s <<endl;
+    cout<<endl <<endl;
     
     
     cout<<"calling with print_vec: ";
     print_vec(stooges);
-    cout<<endl;
+
     
-    cout<<"calling with print_by_ref3: ";
-    
+
     pass_by_ref3(stooges);
+    cout<<"calling with print_by_ref3: ";
+    print_vec(stooges);
+    
+    cout<<endl<<endl;
     
     
     
