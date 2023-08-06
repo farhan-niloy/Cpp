075b92a80c12e68ca685aa2ea969ea641985c165
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-06 13:45:55 +0600

    add arrys&functions

---
+++ Arrys&Function/Arrys&Function/main.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  Arrys&Function
+//
+//  Created by Farhan Niloy on 6/8/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void print_arry(int arry[], size_t size);
+void set_arry(int arry[], size_t size, int value);
+
+int main() {
+    
+    int scores[] {100, 300, 500, 600, 90, 50};
+    
+    print_arry(scores, 5);
+    set_arry(scores, 5, 100);
+    cout<<endl;
+    print_arry(scores, 5);
+    
+    
+    
+   
+    return 0;
+}
+
+
+void print_arry(int arry[], size_t size) {
+    for(size_t i{0}; i<size; i++)
+        cout<<arry[i] <<endl;
+};
+
+void set_arry(int arry[], size_t size, int value) {
+    for(size_t i{0}; i<size; i++)
+        arry[i]=value;
+}
