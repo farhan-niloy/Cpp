996a0c491b6958e5dc9c09737f752b512d38a09d
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-06 16:40:47 +0600

    done

--- How Function Call Work/How Function Call Work/main.cpp
+++ How Function Call Work/How Function Call Work/main.cpp
@@ -9,7 +9,26 @@
 
 using namespace std;
 
+void func2(int &z, int a, int b){
+    z+= a+b;
+}
+
+int func1(int x, int y){
+    int result{};
+    result=x+y;
+    func2(result, x, y);
+    return result;
+}
+
 int main() {
-   
+    int x{10};
+    int y{20};
+    int z{};
+    
+    z = func1(x, y);
+    
+    cout<<z <<endl;
+    
+    
     return 0;
 }
