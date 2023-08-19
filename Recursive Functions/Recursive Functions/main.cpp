3d809783828247c7e96a548922931743977ec562
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-19 12:34:02 +0600

    add factorials

--- Recursive Functions/Recursive Functions/main.cpp
+++ Recursive Functions/Recursive Functions/main.cpp
@@ -19,12 +19,30 @@ unsigned long long fibonacci(unsigned long long n){
     return fibonacci(n-1)+fibonacci(n-2);
 }
 
+unsigned long long factorial(unsigned long long);
+
+unsigned long long factorial(unsigned long long n){
+   if (n==0)
+       return 1;
+    return n*factorial(n-1);
+}
+
 int main() {
     
-    vector<int>vec{5,30,40};
+    vector<int>vecFib{5,30,40};
+    vector<int>vecFac{3, 8, 12, 20};
     
-    for(int i{0}; i<= vec.size()-1; i++){
-        cout<<fibonacci(vec[i]) <<endl;
+    
+    cout<<"Printing fibonacci numbers: " <<endl;
+    
+    for(int i{0}; i<= vecFib.size()-1; i++){
+        cout<<fibonacci(vecFib[i]) <<endl;
+    }
+    
+    cout<< "\nPrinting factorial numbers: " <<endl;
+    
+    for (int i{0}; i<=vecFac.size()-1; i++) {
+        cout<<factorial(vecFac[i]) <<endl;
     }
     
     
