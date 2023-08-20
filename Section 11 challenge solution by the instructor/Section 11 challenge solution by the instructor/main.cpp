ded17d6f4dd602ed812a81a1f146f9ee83b6141b
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-20 14:35:18 +0600

    add add number function

--- Section 11 challenge solution by the instructor/Section 11 challenge solution by the instructor/main.cpp
+++ Section 11 challenge solution by the instructor/Section 11 challenge solution by the instructor/main.cpp
@@ -13,6 +13,16 @@ using namespace std;
 void display_menu();
 char get_selection();
 
+void handle_display(const vector<int> &v);
+void display_list(const vector<int> &v);
+
+void handle_add(vector<int> &v);
+
+void calc_mean(const vector<int> &v);
+
+
+
+
 
 int main() {
     
@@ -22,7 +32,23 @@ int main() {
     do {
         display_menu();
         selection = get_selection();
-        add_numbers();
+        switch (selection) {
+            case 'P':
+                handle_display(numbers);
+                break;
+            case 'A':
+                handle_add(numbers);
+                break;
+            case 'M':
+                calc_mean(numbers);
+                break;
+                
+            default:
+                handle_unknown();
+                break
+                
+        }
+        
         
     } while (selection !='Q');
     
@@ -38,6 +64,7 @@ void display_menu(){
     cout<<"S - The smallest number: " <<endl;
     cout<<"L - The largest number: " <<endl;
     cout<<"-----------------------" <<endl;
+    cout<<"Enter your choice: " <<endl;
 }
 
 char get_selection(){
@@ -45,3 +72,27 @@ char get_selection(){
     cin>>c;
     return toupper(c);
 }
+
+void handle_display(const vector<int> &v){
+    if (v.size()==0)
+        cout<<"[] - The list it empty! " <<endl;
+    else
+        display_list(v);
+}
+
+void display_list(const vector<int> &v){
+    cout<<"[";
+    for(auto num:v)
+        cout<<num <<" ";
+    cout<<"]" <<endl;
+}
+
+void handle_add(vector<int> &v){
+    int num_to_add{};
+    cout<<"Enter a number to add: ";
+    cin>>num_to_add;
+    v.push_back(num_to_add);
+    cout<<num_to_add <<"Added" <<endl;
+}
+
+void calc_mean(const vector<int> &v);
