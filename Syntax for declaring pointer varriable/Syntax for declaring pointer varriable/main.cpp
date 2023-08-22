9483988b38cea7667a7a8c44ae02a0e64d1dfd0d
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-22 13:35:18 +0600

    initial commit

---
+++ Syntax for declaring pointer varriable/Syntax for declaring pointer varriable/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Syntax for declaring pointer varriable
+//
+//  Created by Farhan Niloy on 22/8/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int num {10};
+    
+    cout<<"The value of num is: " <<num <<endl;
+    cout<<"The sizeof num is: " <<sizeof num <<endl;
+    cout<<"The adressof num is: " <<&num <<endl;
+    
+    return 0;
+}
