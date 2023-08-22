ed5448428c38ce1ee4e35fefa0b5fcc6e6afbe8d
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-22 13:48:35 +0600

    size in my machine is 8

--- Syntax for declaring pointer varriable/Syntax for declaring pointer varriable/main.cpp
+++ Syntax for declaring pointer varriable/Syntax for declaring pointer varriable/main.cpp
@@ -29,5 +29,19 @@ int main(int argc, const char * argv[]) {
     cout<<"The sizeof num is: " <<sizeof p <<endl;
     cout<<"The adressof num is: " <<&p <<endl <<endl;
     
+    
+    int *p1 {nullptr};
+    double *p2 {nullptr};
+    unsigned long long *p3 {nullptr};
+    vector <string> *p4 {nullptr};
+    string *p5 {nullptr};
+    
+    cout<<"\nsize of p1 is: " <<sizeof p1 <<endl;
+    cout<<"\nsize of p2 is: " <<sizeof p2 <<endl;
+    cout<<"\nsize of p3 is: " <<sizeof p3 <<endl;
+    cout<<"\nsize of p4 is: " <<sizeof p4 <<endl;
+    cout<<"\nsize of p5 is: " <<sizeof p5 <<endl;
+        
+    
     return 0;
 }
