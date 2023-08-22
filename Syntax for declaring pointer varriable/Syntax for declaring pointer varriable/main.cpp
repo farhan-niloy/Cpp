c4f9a4f502779595574e6cad27647ec0fbbaca4a
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-22 13:58:34 +0600

    add some more details

--- Syntax for declaring pointer varriable/Syntax for declaring pointer varriable/main.cpp
+++ Syntax for declaring pointer varriable/Syntax for declaring pointer varriable/main.cpp
@@ -40,7 +40,21 @@ int main(int argc, const char * argv[]) {
     cout<<"\nsize of p2 is: " <<sizeof p2 <<endl;
     cout<<"\nsize of p3 is: " <<sizeof p3 <<endl;
     cout<<"\nsize of p4 is: " <<sizeof p4 <<endl;
-    cout<<"\nsize of p5 is: " <<sizeof p5 <<endl;
+    cout<<"\nsize of p5 is: " <<sizeof p5 <<endl <<endl;
+    
+    
+    int score{10};
+    double high_temp{100.4};
+    
+    int *score_ptr{nullptr};
+    
+    score_ptr = &score;
+    
+    cout<<"Value of score is: " <<score <<endl;
+    cout<<"Sizeof score is: " <<sizeof score <<endl;
+    cout<<"value of score_ptr is:" <<score_ptr <<endl;
+    
+    
         
     
     return 0;
