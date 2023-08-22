d6dde7f64e5f5bd9eb5d5ef5a6d8bc7da175df7f
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-22 13:40:51 +0600

    add pointer value, adress, size

--- Syntax for declaring pointer varriable/Syntax for declaring pointer varriable/main.cpp
+++ Syntax for declaring pointer varriable/Syntax for declaring pointer varriable/main.cpp
@@ -15,7 +15,19 @@ int main(int argc, const char * argv[]) {
     
     cout<<"The value of num is: " <<num <<endl;
     cout<<"The sizeof num is: " <<sizeof num <<endl;
-    cout<<"The adressof num is: " <<&num <<endl;
+    cout<<"The adressof num is: " <<&num <<endl <<endl;
+    
+    int *p;
+    
+    cout<<"The value of num is: " <<p <<endl;
+    cout<<"The sizeof num is: " <<sizeof p <<endl;
+    cout<<"The adressof num is: " <<&p <<endl <<endl;
+    
+    p = nullptr;
+    
+    cout<<"The value of num is: " <<p <<endl;
+    cout<<"The sizeof num is: " <<sizeof p <<endl;
+    cout<<"The adressof num is: " <<&p <<endl <<endl;
     
     return 0;
 }
