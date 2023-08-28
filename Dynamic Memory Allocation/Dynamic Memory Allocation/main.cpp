37e139d1b1da4908356ccb20375750e83291c3b5
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-28 16:55:08 +0600

    Using delete to deallocate storage

--- Dynamic Memory Allocation/Dynamic Memory Allocation/main.cpp
+++ Dynamic Memory Allocation/Dynamic Memory Allocation/main.cpp
@@ -7,8 +7,28 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    int *int_ptr {nullptr};
+    int_ptr = new int;
+    cout<<int_ptr <<endl;
+    delete int_ptr;
+    
+    size_t size{0};
+    double *temp_ptr{nullptr};
+    
+    cout<<"How many temps?: ";
+    cin>>size;
+    
+    temp_ptr = new double [size];
+    
+    cout<<temp_ptr <<endl;
+    
+    cout<<&temp_ptr <<endl;
+    
+    delete [] temp_ptr;
+   
     return 0;
 }
