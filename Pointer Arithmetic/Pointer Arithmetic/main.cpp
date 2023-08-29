49d72cc6edf1cf14b6d8afb36e7fe24ebed82bfa
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-29 14:32:22 +0600

    add pointer arithmetic

--- Pointer Arithmetic/Pointer Arithmetic/main.cpp
+++ Pointer Arithmetic/Pointer Arithmetic/main.cpp
@@ -6,8 +6,30 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
+    
+    int scores [] {100,98, 94, 68, 33, -1};
+    int *score_ptr{scores};
+    
+    while(*score_ptr!= -1) {
+        cout<<*score_ptr<<endl;
+        score_ptr++;
+        
+        cout<<endl;
+    }
+    
+    score_ptr=scores;
+    
+    cout<<"\n......................" <<endl;;
+    
+    while(*score_ptr != -1) {
+        cout<<*score_ptr++<<endl;
+        cout<<endl;
+    }
+    
+   
     
     return 0;
 }
