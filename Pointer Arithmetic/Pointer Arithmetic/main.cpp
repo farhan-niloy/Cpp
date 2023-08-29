cc53aec94b5acd032dbf90bbe02270482d3981ae
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-29 14:48:56 +0600

    add pointer arithmetic

--- Pointer Arithmetic/Pointer Arithmetic/main.cpp
+++ Pointer Arithmetic/Pointer Arithmetic/main.cpp
@@ -29,7 +29,32 @@ int main(int argc, const char * argv[]) {
         cout<<endl;
     }
     
-   
+    cout<<"\n........................." <<endl;
+    
+    string s1 {"Farhan"};
+    string s2 {"Niloy"};
+    string s3 {"Farhan"};
+    
+    string *p1 {&s1};
+    string *p2 {&s2};
+    string *p3 {&s1};
+    
+    cout<<boolalpha;
+    cout<<p1 <<" == " <<p2 <<": " <<(p1==p2) <<endl;
+    cout<<p1 <<" == " <<p3 <<": " <<(p1==p3) <<endl;
+    cout<<endl;
+    
+    cout<<*p1 <<" == " <<*p2 <<": " <<(*p1==*p2) <<endl;
+    cout<<*p1 <<" == " <<p3 <<": " <<(*p1==*p3) <<endl;
+    cout<<endl;
+    
+    p3=&s3;
+    cout<<*p1 <<" == " <<*p3 <<": " <<(*p1==*p3) <<endl;
+    
+    
+    
+
+    
     
     return 0;
 }
