96fd53d93768edcf858b27061d31d7c1fca5171f
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-29 13:47:58 +0600

    initial commit

---
+++ Pointer Arithmetic/Pointer Arithmetic/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  Pointer Arithmetic
+//
+//  Created by Farhan Niloy on 29/8/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
