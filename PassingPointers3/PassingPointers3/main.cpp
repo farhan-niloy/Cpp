beb2f433dc3d04ee026703416517c7eb771c0dc7
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-09-08 14:04:14 +0600

    add passing pointers 3

--- PassingPointers3/PassingPointers3/main.cpp
+++ PassingPointers3/PassingPointers3/main.cpp
@@ -1,29 +1,49 @@
+////
+////  main.cpp
+////  PassingPointers3
+////
+////  Created by Farhan Niloy on 8/9/23.
+////
 //
-//  main.cpp
-//  PassingPointers3
-//
-//  Created by Farhan Niloy on 8/9/23.
-//
-
 #include <iostream>
 #include <vector>
 #include <string>
 
 using namespace std;
+//
+//void display (vector<string> *const v){
+//   // (*v).at(0)="Funny";
+//
+//    for(auto str: *v)
+//        cout<<str<<endl;
+//}
+//
+//int main(int argc, const char * argv[]){
+//
+//    vector<string>stooges {
+//        "Larry, " "Curly, " "Moe"
+//    };
+//
+//    display(&stooges);
+//
+//    return 0;
+//}
 
-void display (vector<string> *v){
-    
-    for(auto str: *v)
-        cout<<str<<endl;
+
+void display (int *arry, int sentinel) {
+    while(*arry != sentinel) {
+        cout<<*arry++ <<" ";
+        cout<<endl;
+    }
 }
 
-int main(int argc, const char * argv[]){
+
+int main () {
     
-    vector<string>stooges {
-        "Larry, " "Curly, " "Moe"
-    };
+    int scores[] {100, 98, 97, 79, 85, -1};
+    display(scores, -1);
     
-    display(&stooges);
+    cout<<endl;
     
     return 0;
 }
