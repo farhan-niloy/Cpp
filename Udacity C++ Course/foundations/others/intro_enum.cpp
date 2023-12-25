554cb7ad34097498eb8c7240736238aca79551c5
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-12-25 05:59:47 -0500

    update

--- Udacity C++ Course/foundations/others/intro_enum.cpp
+++ Udacity C++ Course/foundations/others/intro_enum.cpp
@@ -32,9 +32,10 @@ int main()
     // Note: for the switch statement, don't forget the "break" command
     switch (a) {
       case Direction::kUp : cout << "Going up!" << "\n";
-        break;
-      case Direction::kDown : cout << "Going down!" << "\n";
-        break;
+        break
+
+        case Direction::kDown : cout << "Going down!" << "\n";
+            break;
       case Direction::kLeft : cout << "Going left!" << "\n";
         break;
       case Direction::kRight : cout << "Going right!" << "\n";
