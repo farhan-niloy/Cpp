7feadb64e73a5a030987ab6bf4c14d5ceb0ba59c
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-12-31 07:58:51 -0500

    update

---
+++ C++ From Beginer to Beyond/oop/constructor_and_deconstructor.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+
+class Player {
+private:
+    std::string name;
+    int health;
+    int xp;
+
+public:
+    void set_name(std::string n) {name = n;}
+
+    Player(){
+        cout<<"No arg constructor called!" <<endl;
+    }
+    Player(std::string name){
+        cout<<"String arg constructor called!" <<endl;
+    }
+    Player(std::string name, int health, int xp) {
+        cout<<"Three arg constructor called!" <<name <<endl;
+    }
+
+    ~Player(){
+        cout<<"Destructor called" <<endl;
+    }
+};
+
+int main(){
+
+    {
+        Player slayer;
+        slayer.set_name("Kagura");
+    }
+    {
+        Player shinpachi;
+        shinpachi.set_name("Shinpachi");
+        Player Sadaharu("Sadaharu");
+        Sadaharu.set_name("Sadaharu");
+        Player bigsis("Bigsis", 100, 13);
+        bigsis.set_name("bigsis")
+
+    }
+
+
+    return 0;
+}
