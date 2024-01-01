73450b9e8b949d99d5e90dad3930f3dc5f7b393b
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-01 04:57:40 -0500

    update

---
+++ C++ From Beginer to Beyond/oop/delegating_constructor.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+
+class Player {
+private:
+    std::string name;
+    int health;
+    int xp;
+public:
+    Player();
+    Player(std::string string_val);
+    Player(std::string string_val, int int_val, int xp_val);
+};
+
+Player::Player()
+    : name{None}, health{0}, xp{0} {
+}
+
+Player::Player(std::string string_val) :
+    name{string_val}, health{0}, xp{0}{
+}
+
+Player::Player(std::string string_val, int int_val, int xp_val) : name{string_val}, {
+
+}
+
+int main(){
+
+    return 0;
+}
