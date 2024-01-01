ab16bee7b524ac13a1bf9123c071a1272bf2d553
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-01 05:24:12 -0500

    update

--- C++ From Beginer to Beyond/oop/delegating_constructor.cpp
+++ C++ From Beginer to Beyond/oop/delegating_constructor.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 using std::string;
+using std::cout;
 
 class Player {
 private:
@@ -19,16 +20,19 @@ Player::Player()
 }
 
 Player::Player(std::string string_val)
-    :Player{string_val, 0, 0}{
+    :Player{string_val, 0, 0} {
 }
 
 Player::Player(std::string string_val, int health_val, int xp_val)
     :name{string_val}, health{health_val}, xp{xp_val} {
-
+    cout<<"name = " <<name <<" health = " <<health << " xp = " <<xp <<"\n";
 }
 
 int main(){
 
+    Player empty;
+    Player gin{"Gin"};
+    Player sadaharu{"Sadaharu", 100, 1000};
 
     return 0;
 }
