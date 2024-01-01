59d76850a92cf86f20d0996673473a65b00fba4b
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-01 06:03:33 -0500

    update

---
+++ C++ From Beginer to Beyond/oop/constructor_parameters_n_default_values.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+class Player{
+private:
+    std::string name;
+    int health;
+    int xp;
+public:
+    Player(std::string name_val = "none", int health_val = 0, int xp_val = 0);
+};
+
+Player::Player(std::string name_val, int health_val, int xp_val) :name{name_val}, health{health_val}, xp{xp_val} {
+    std::cout<<"Three-args constructor";
+}
+
+int main() {
+
+
+    return 0;
+}
