a5d2db46186fc2857efae1eb986d16a9450f7f68
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-03 06:36:51 -0500

    update

---
+++ C++ From Beginer to Beyond/oop/deep_copy.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using std::endl;
+
+class Deep{
+private:
+    int *data;
+
+public:
+    Deep (int d);
+    Deep (const Deep &source);
+    ~Deep();
+};
+
+Deep
+
+int main(){
+
+    return 0;
+}
