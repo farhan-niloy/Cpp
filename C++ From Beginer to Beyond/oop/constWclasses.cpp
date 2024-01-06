1fcf041c0907f5ab2c7333c583d46a7bdcbdad23
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-06 05:46:18 -0500

    update

--- C++ From Beginer to Beyond/oop/constWclasses.cpp
+++ C++ From Beginer to Beyond/oop/constWclasses.cpp
@@ -13,8 +13,16 @@ private:
     int xp;
 
 public:
-    void
+    void set_name(std::string name_val) {
+        name = name_val;
+    }
 
+    std::string get_name() const {return name;}
+
+    //Overloaded constructors
+    Player();
+    Player(std::string name_val);
+    Player(std::string name_val, int health_val, int xp_val);
 };
 
 int main(){
