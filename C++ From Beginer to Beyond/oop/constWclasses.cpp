0362b9f4d16bbcd5d2ba6985b0c6a1cd6881552f
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-06 05:42:12 -0500

    update

---
+++ C++ From Beginer to Beyond/oop/constWclasses.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::endl;
+using std::cout;
+
+class Player {
+private:
+    std::string name;
+    int health;
+    int xp;
+
+public:
+    void
+
+};
+
+int main(){
+
+    return 0;
+}
