67db4d0437b803da71ca12eb9c736a47e22bad58
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-09 03:46:56 -0500

    update

--- C++ From Beginer to Beyond/oop/static_class_member/Player.cpp
+++ C++ From Beginer to Beyond/oop/static_class_member/Player.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "Player.h"
+
+int Player::num_player{0};
+
+Player::Player(std::string name_val, int health_val, int xp_val)
+    :name{name_val}, health{health_val}, xp{xp_val} {
+
+}
--- C++ From Beginer to Beyond/oop/static_class_member/Player.h
+++ C++ From Beginer to Beyond/oop/static_class_member/Player.h
@@ -9,6 +9,7 @@
 
 class Player{
 private:
+    static int num_player;
     std::string name;
     int health;
     int xp;
@@ -17,6 +18,12 @@ public:
     int get_health(){return health;}
     int get_xp(){return xp;}
     //constructor
+    Player(std::string name_val = "none", int health_val = 0, int xp_val = 0);
+    Player(const Player &source);
+    //destructor
+    ~Player();
+
+    static int get_num_player();
 };
 
 
