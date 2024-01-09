f70609fd98284a320e4278b6f9cdd9dc87ccf7a1
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-09 03:50:14 -0500

    update

--- C++ From Beginer to Beyond/oop/static_class_member/Player.cpp
+++ C++ From Beginer to Beyond/oop/static_class_member/Player.cpp
@@ -8,5 +8,12 @@ int Player::num_player{0};
 
 Player::Player(std::string name_val, int health_val, int xp_val)
     :name{name_val}, health{health_val}, xp{xp_val} {
+}
+
+Player::Player(const Player &source)
+    :Player{source.name, source.health, source.xp}{
+}
+
+Player::~Player(){
 
 }
