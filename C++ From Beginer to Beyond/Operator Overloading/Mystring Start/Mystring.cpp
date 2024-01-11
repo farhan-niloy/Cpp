17b830e040ba5b8b5e262b133d379a95e4490bf9
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-11 08:44:25 -0500

    update

--- C++ From Beginer to Beyond/Operator Overloading/Mystring Start/Mystring.cpp
+++ C++ From Beginer to Beyond/Operator Overloading/Mystring Start/Mystring.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "Mystring.h"
+#include <iostream>
+#include <cstring>
+
+Mystring::Mystring()
+    :str(nullptr) {
+    str = new char[1];
+    *str = '\0';
+}
+
+Mystring::Mystring(const char *s)
+    :str{nullptr}{
+    if(s == nullptr) {
+        str = new char[1];
+        *str = '\0';
+    }else {
+        str = new char[std::strlen(s)+1];
+        std::strcpy(str, s);
+    }
+}
+
+
--- C++ From Beginer to Beyond/Operator Overloading/Mystring Start/Mystring.h
+++ C++ From Beginer to Beyond/Operator Overloading/Mystring Start/Mystring.h
@@ -13,7 +13,12 @@ private:
 public:
     Mystring();
     Mystring(const char *s);
+    Mystring(const Mystring &source);
+    ~Mystring()
 
+    void display() const;
+    int get_length() const;
+    const char *get_str() const;
 };
 
 
