af8143a113778ca7d3470558feb189766d4823ff
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-11 12:46:46 -0500

    update

--- C++ From Beginer to Beyond/Operator Overloading/untitled/main.cpp
+++ C++ From Beginer to Beyond/Operator Overloading/untitled/main.cpp
@@ -2,13 +2,20 @@
 #include "Mystring.h"
 
 int main() {
-    Mystring empty;               //no arg constructor
-    Mystring larry{"Larry"};   //overloaded constructor
-    Mystring stooges{larry};      //copy constructor
 
-    empty.display();
-    larry.display();
-    stooges.display();
+    Mystring a{"Hello"};        //overloaded constructor
+    Mystring b;                   //no-arg constructor
+    b = a;                        //copy assignment
+                                  //b.operator=(a)
+    b = "Hello world!";           //b.operator=("Hello world!")
+
+//    Mystring empty;               //no-arg constructor
+//    Mystring larry{"Larry"};   //overloaded constructor
+//    Mystring stooges{larry};      //copy constructor
+//
+//    empty.display();
+//    larry.display();
+//    stooges.display();
 
     return 0;
 }
