907fee4a4eb0eb6a6d8742c0bac6f27cb1380b64
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-12 13:25:40 -0500

    update

--- C++ From Beginer to Beyond/Operator Overloading/Overloading Operator as Member Function/Mystring.cpp
+++ C++ From Beginer to Beyond/Operator Overloading/Overloading Operator as Member Function/Mystring.cpp
@@ -30,6 +30,23 @@ Mystring::Mystring(const char *s)
 //Copy constructor
 Mystring::Mystring(const Mystring &source)
     :str{nullptr} {
-    str = new char[std::strlen(source.str)]
+    str = new char[std::strlen(source.str)+1];
+    std::strcpy(str, source.str);
 }
+
+//Destructor
+Mystring::~Mystring(){
+    delete [] str;
+}
+
+//Display method
+void Mystring::display() const {
+    std::cout<<str<<" : " <<get_length() <<std::endl;
+}
+
+//length getter
+int Mystring::get_length() const {return std::strlen(str);}
+
+//string getter
+const char *Mystring::get_str() const {return str;}
 
