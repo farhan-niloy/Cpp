0e4b1cc321b38b5e3417a2328d48870ce37d6245
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-12 10:08:23 -0500

    update

--- C++ From Beginer to Beyond/Operator Overloading/Overloading Operator as Member Function/Mystring.h
+++ C++ From Beginer to Beyond/Operator Overloading/Overloading Operator as Member Function/Mystring.h
@@ -18,8 +18,15 @@ public:
     ~Mystring();
 
 
+    Mystring &operator=(const Mystring &rhs);
+    Mystring &operator=(Mystring &&rhs);
+
+    void display() const;
+
+    int get_length() const;
+    const char *get_str() const;
+
 
 };
-
 
 #endif //OVERLOADING_OPERATOR_AS_MEMBER_FUNCTION_MYSTRING_H
