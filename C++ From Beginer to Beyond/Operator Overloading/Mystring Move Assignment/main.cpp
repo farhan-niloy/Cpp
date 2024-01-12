432463bbcf8a75ca0a9d1de9559ccbf081056798
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-12 09:10:10 -0500

    update

--- C++ From Beginer to Beyond/Operator Overloading/Mystring Move Assignment/main.cpp
+++ C++ From Beginer to Beyond/Operator Overloading/Mystring Move Assignment/main.cpp
@@ -7,5 +7,38 @@ int main() {
     a = Mystring{"Hola"};
     a = "Bonjur";
 
+    Mystring empty;                //no-arg constructor
+    Mystring larry{"Larry"};    //overloaded constructor
+    Mystring stooges{larry};       //copy constructor
+
+    empty.display();
+    larry.display();
+    stooges.display();
+    empty.display();
+
+    stooges = "Larry, Moe, and Curly";
+    stooges.display();
+
+    std::vector<Mystring> stooges_vec;
+    stooges_vec.push_back("Larry");
+    stooges_vec.push_back("Moe");
+    stooges_vec.push_back("Curly");
+
+
+    std::cout << "\n==========Loop1==================\n" << std::endl;
+
+    for (const Mystring &s : stooges_vec)
+        s.display();
+
+    std::cout << "\n==========Loop2==================\n"<< std::endl;
+
+    for (Mystring &s : stooges_vec)
+        s = "Changed";
+
+    std::cout << "\n==========Loop3==================\n" << std::endl;
+
+    for (const Mystring &s : stooges_vec)
+        s.display();
+
     return 0;
 }
