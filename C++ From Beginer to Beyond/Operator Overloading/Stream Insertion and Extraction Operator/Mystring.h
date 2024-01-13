1d80c398f9a0717db8d2ab8fe7b0910304e12861
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-13 03:23:14 -0500

    update

--- C++ From Beginer to Beyond/Operator Overloading/Stream Insertion and Extraction Operator/Mystring.h
+++ C++ From Beginer to Beyond/Operator Overloading/Stream Insertion and Extraction Operator/Mystring.h
@@ -1,14 +1,36 @@
 //
-// Created by Niloy Farhan on 13/1/24.
+// Created by Niloy Farhan on 12/1/24.
 //
 
-#ifndef STREAM_INSERTION_AND_EXTRACTION_OPERATOR_MYSTRING_H
-#define STREAM_INSERTION_AND_EXTRACTION_OPERATOR_MYSTRING_H
+#ifndef OVERLOADING_OPERATOR_AS_MEMBER_FUNCTION_MYSTRING_H
+#define OVERLOADING_OPERATOR_AS_MEMBER_FUNCTION_MYSTRING_H
 
 
-class Mystring {
+class Mystring{
+private:
+    char *str;
+
+public:
+    Mystring();
+    Mystring(const char *s);
+    Mystring(const Mystring &source);
+    Mystring(Mystring &&source);
+    ~Mystring();
+
+
+    Mystring &operator=(const Mystring &rhs);       //Copy assignment
+    Mystring &operator=(Mystring &&rhs);        //Move assignment
+
+    Mystring operator-() const;     //Make lowercase
+    Mystring operator+(const Mystring &rhs) const;      //Concatenate
+    bool operator==(const Mystring &rhs) const;     //Compare
+
+    void display() const;
+
+    int get_length() const;
+    const char *get_str() const;
+
 
 };
 
-
-#endif //STREAM_INSERTION_AND_EXTRACTION_OPERATOR_MYSTRING_H
+#endif //OVERLOADING_OPERATOR_AS_MEMBER_FUNCTION_MYSTRING_H
--- C++ From Beginer to Beyond/Operator Overloading/Stream Insertion and Extraction Operator/main.cpp
+++ C++ From Beginer to Beyond/Operator Overloading/Stream Insertion and Extraction Operator/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "Mystring.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Mystring larry{"Larry"};
+    Mystring moe{"Moe"};
+    Mystring curly;
+
+    std::cout<<"Enter the third stooge's first name: ";
+    std::cin>> curly;
+
+    std::cout<<"The three stooges are " <<larry <<"," <<curly <<"," <<moe <<std::endl;
+
+
     return 0;
 }
