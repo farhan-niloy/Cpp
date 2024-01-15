f7c3889d3523dd31fc5da17df86e258a6ba21273
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-15 06:45:48 -0500

    update

--- C++ From Beginer to Beyond/Inheritance/Redefining Base Class Method/Savings_Account.h
+++ C++ From Beginer to Beyond/Inheritance/Redefining Base Class Method/Savings_Account.h
@@ -4,9 +4,17 @@
 
 #ifndef REDEFINING_BASE_CLASS_METHOD_SAVINGS_ACCOUNT_H
 #define REDEFINING_BASE_CLASS_METHOD_SAVINGS_ACCOUNT_H
+#include "Account.h"
 
 
-class Savings_Account {
+class Savings_Account:public Account {
+    friend std::ostream &operator<<(std::ostream &os, const Savings_Account &account);
+protected:
+    double int_rate;
+public:
+    Savings_Account();
+    Savings_Account(double balance, double int_rate);
+    void deposit(double amount);
 
 };
 
--- C++ From Beginer to Beyond/Inheritance/Redefining Base Class Method/main.cpp
+++ C++ From Beginer to Beyond/Inheritance/Redefining Base Class Method/main.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include "Account.h"
+#include "Savings_Account.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout<<"\n=====Account Class==========================" <<std::endl;
+    Account a1{1000.0};
+    std::cout<<a1<<std::endl;
+
+    a1.deposit(500.0);
+    std::cout<<a1<<std::endl;
+
+    a1.withdraw(1000);
+    std::cout<<a1<<std::endl;
+
+    a1.withdraw(10000);
+    std::cout<<a1<<std::endl;
+
+    std::cout<<"\n=====Savings Account Class=======================" <<std::endl;
+    Savings_Account s1{1000, 5.0};
+    std::cout <<s1 <<std::endl;
+
+    s1.deposit(1000);
+    std::cout<<s1<<std::endl;
+
+    s1.withdraw(2000);
+    std::cout<<s1<<std::endl;
+
+    s1.withdraw(1000);
+    std::cout<<s1<<std::endl;
+
     return 0;
 }
