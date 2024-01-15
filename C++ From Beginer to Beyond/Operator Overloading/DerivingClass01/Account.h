8e21af88073c03c91fd68127b04a50a434af66ee
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-15 02:41:05 -0500

    update

---
+++ C++ From Beginer to Beyond/Operator Overloading/DerivingClass01/Account.h
@@ -0,0 +1,14 @@
+//
+// Created by Niloy Farhan on 15/1/24.
+//
+
+#ifndef DERIVINGCLASS01_ACCOUNT_H
+#define DERIVINGCLASS01_ACCOUNT_H
+
+
+class Account {
+
+};
+
+
+#endif //DERIVINGCLASS01_ACCOUNT_H
