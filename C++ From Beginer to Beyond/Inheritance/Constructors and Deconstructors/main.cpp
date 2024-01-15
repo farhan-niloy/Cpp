9203ef9cf3a0bb1e4a68fef28c134c507ef2e36c
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-15 04:07:09 -0500

    update

--- C++ From Beginer to Beyond/Inheritance/Constructors and Deconstructors/main.cpp
+++ C++ From Beginer to Beyond/Inheritance/Constructors and Deconstructors/main.cpp
@@ -9,11 +9,25 @@ private:
 public:
     Base():value{0}{cout<<"Base no-args constructor called" <<endl;};
     Base(int x): value{x}{cout<<"Base (int) overloaded constructor called" <<endl;};
-    ~Base();
+    ~Base(){cout<<"Base destructor called"<<endl;};
+};
 
+class Derived:public Base {
+    using Base::Base;
+private:
+    int double_value;
+public:
+    Derived():double_value{0} {cout<<"Derived no-args constructor called" <<endl;};
+    Derived(int x): double_value{x*2} {cout<<"Derived (int) overloaded constructor called" <<endl;};
+    ~Derived() {cout<<"Derived destructor called"<<endl;} ;
 };
 
 int main() {
+    Base b;
+    Base c{200};
+
+    Derived a{};
+    Derived d{100};
 
 
     return 0;
