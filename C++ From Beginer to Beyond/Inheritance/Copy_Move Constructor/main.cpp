ba885366a7e932270eea9f7fca1da455b034149f
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-15 05:33:45 -0500

    update

--- C++ From Beginer to Beyond/Inheritance/Copy_Move Constructor/main.cpp
+++ C++ From Beginer to Beyond/Inheritance/Copy_Move Constructor/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+using std::cout;
+using std::endl;
+
+class Base{
+private:
+    int value;
+public:
+    Base()
+        :value{0}{
+        cout<<"Base no-args constructor" <<endl;
+    }
+    Base(int x)
+        :value{x}{
+        cout<<"int Base constructor" <<endl;
+    }
+};
+
+class Derived{
+
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
