5e9c66849270206fd71b15687a948676120562b4
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-15 04:46:45 -0500

    update

--- C++ From Beginer to Beyond/Inheritance/Base Class Initialization/main.cpp
+++ C++ From Beginer to Beyond/Inheritance/Base Class Initialization/main.cpp
@@ -4,11 +4,28 @@ using std::cout;
 using std::endl;
 
 class Base{
-
+private:
+    int value;
+public:
+    Base():value{0}{cout<<"Base no-args constructor" <<endl;};
+    Base(int x):value{x}{cout<<"Base (int) overloaded constructor" <<endl;};
 };
 
-class Derived{
-
+class Derived: public Base {
+private:
+    int double_value;
+public:
+    Derived()
+        :Base{}, double_value{0}{
+        cout<<"Derived no-args constructor" <<endl;
+    }
+    Derived(int x)
+        :Base{x}, double_value{x*2} {
+        cout<<"Derived (int) constructor " <<endl;
+    }
+    ~Derived(){
+        cout<<"Derived destructor" <<endl;
+    }
 };
 
 int main() {
