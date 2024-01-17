e4a57178c650b5318b3ba49062bee1c855cbb878
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-17 05:41:27 -0500

    update

---
+++ C++ From Beginer to Beyond/Polymorphism/Override/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+class Base{
+public:
+    virtual void say_hello() const{
+        std::cout<<"Hello - I'm a Base class object" <<std::endl;
+    }
+    virtual ~Base(){std::cout<<"Destructor Base" <<std::endl;}
+};
+
+class Derived:public Base{
+public:
+    virtual void say_hello(){
+        std::cout<<"Hello - I'm a Derived class object" <<std::endl;
+    }
+    virtual ~Derived(){std::cout<<"Destructor Derived" <<std::endl;}
+};
+
+int main() {
+    Base *p1 = new Base();
+    p1->say_hello();
+
+    Derived *p2 = new Derived;
+    p2->say_hello();
+
+    Base *p3 = new Derived();
+    p3->say_hello();
+
+    delete p1;
+    delete p2;
+    delete p3;
+
+
+    return 0;
+}
