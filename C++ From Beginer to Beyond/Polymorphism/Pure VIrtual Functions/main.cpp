9b94eb12ecf914ea13aa4189d43dc74227a8b2a3
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-17 06:29:23 -0500

    update

---
+++ C++ From Beginer to Beyond/Polymorphism/Pure VIrtual Functions/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+class Shape{    //Abstract Base Class
+public:
+    virtual void draw() = 0;
+    virtual void rotate() = 0;
+    virtual ~Shape(){}
+
+};
+class Open_Shape: public Shape{
+public:
+    virtual ~Open_Shape(){}
+};
+
+class CLosed_Shape: public Shape{
+public:
+    virtual ~CLosed_Shape(){}
+};
+
+class Line:public Open_Shape{
+    virtual void draw() override{
+        std::cout<<"Drawing a Line" <<std::endl;
+    }
+    virtual void rotate() override{
+        std::cout<<"Rotating a Line" <<std::endl;
+    }
+    virtual ~Line(){}
+};
+
+class Circle: public CLosed_Shape {
+public:
+    virtual void draw() override{
+        std::cout<<"Drawing a Circle" <<std::endl;
+    }
+    virtual void rotate() override{
+        std::cout<<"Rotating a Circle" <<std::endl;
+    }
+    virtual ~Circle(){}
+};
+
+class Square: public CLosed_Shape{
+public:
+    virtual void draw() override {
+        std::cout<<"Drawing a Square" <<std::endl;
+    }
+    virtual void rotate() override {
+        std::cout<<"Rotating a Square" <<std::endl;
+    }
+    virtual ~Square(){}
+};
+
+
+int main() {
+//    Shape s;
+//    Shape *p = new Shpae();
+
+
+      Shape *ptr = new Circle();
+      ptr-> draw();
+      ptr->rotate();
+
+
+    return 0;
+}
