de725ef12ea7094be20d31ab976941ee75571b65
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-17 05:20:56 -0500

    update

--- C++ From Beginer to Beyond/Polymorphism/Virtual Function/main.cpp
+++ C++ From Beginer to Beyond/Polymorphism/Virtual Function/main.cpp
@@ -27,7 +27,22 @@ class Trust: public Account{
 
 int main() {
 
-    std::cout<<"\n===Pointers======"
+    std::cout<<"\n===Pointers======" <<std::endl;
+    Account *p1 = new Account();
+    Account *p2 = new Savings();
+    Account *p3 = new Checking();
+    Account *p4 = new Trust();
+
+    p1->withdraw(1000);
+    p2->withdraw(1000);
+    p3->withdraw(1000);
+    p4->withdraw(1000);
+
+    std::cout<<"\n==== Clean up=======" <<std::endl;
+    delete p1;
+    delete p2;
+    delete p3;
+    delete p4;
 
     return 0;
 }
