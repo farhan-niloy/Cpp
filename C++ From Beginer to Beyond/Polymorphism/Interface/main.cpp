679b97bbb3c676c405ba06bf1c83036bbfe2d22a
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-18 04:42:40 -0500

    update

--- C++ From Beginer to Beyond/Polymorphism/Interface/main.cpp
+++ C++ From Beginer to Beyond/Polymorphism/Interface/main.cpp
@@ -1,29 +1,44 @@
 #include <iostream>
 
-class Account{
-    friend std::ostream &operator<<(std::ostream &os, const Account &acc);
+class I_Printable {
+    friend std::ostream &operator<<(std::ostream &os, const I_Printable &obj);
+public:
+    virtual void print(std::ostream &os) const = 0;
+};
+
+std::ostream &operator<<(std::ostream &os, const I_Printable &obj){
+    obj.print(os);
+    return os;
+}
+
+class Account:public I_Printable{
 public:
     virtual void withdraw(double amount) {
         std::cout << "In Account::withdraw" << std::endl;
     }
+    virtual void print(std::ostream &os) const {
+        os<<"Account display";
+    }
     virtual ~Account(){}
 };
 
-std::ostream &operator<<(std::ostream &os, const Account &acc){
-    os<<"Account display";
-    return os;
-}
-
 class Checking: public Account{
-    friend std::ostream &operator<<(std::ostream &os, const Account &account);
 public:
     virtual void withdraw(double amount){
         std::cout<<"In Checking::withdraw" <<std::endl;
+    }
+    virtual void print(std::ostream &os) const {
+        os<<"Checking display";
     }
     virtual ~Checking(){}
 };
 
 int main() {
+    Account *p1 = new Account();
+    std::cout<<*p1<<std::endl;
+
+    Account *p2 = new Checking();
+    std::cout<<*p2<<std::endl;
 
     return 0;
 }
