d0bff80a217013ade5409a86f266f75410562572
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-20 08:12:34 -0500

    update

--- C++ From Beginer to Beyond/Smart Pointers/Shared Pointer/main.cpp
+++ C++ From Beginer to Beyond/Smart Pointers/Shared Pointer/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <vector>
 
 int main() {
     std::shared_ptr<int> p1 {new int{100}};
@@ -12,6 +13,17 @@ int main() {
     std::cout<<p1.use_count() <<std::endl;
     std::cout<<p2.use_count() <<std::endl;
 
+    std::vector<std::shared_ptr<int>> vec;
+
+    std::shared_ptr<int> ptr{new int{100}};
+    vec.push_back(ptr);
+
+    std::cout<<ptr.use_count() <<std::endl;
+
+    std::shared_ptr<int>pp1 = std::make_shared<int>(100);
+    std::shared_ptr<int> pp2{pp1};
+    std::shared_ptr<int> pp3;
+    pp3=pp1;
 
     return 0;
 }
