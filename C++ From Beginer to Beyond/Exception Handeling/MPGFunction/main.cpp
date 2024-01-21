5d73acfcd7cbd1838e658bda4415327c8d251edb
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-21 03:17:59 -0500

    update

--- C++ From Beginer to Beyond/Exception Handeling/MPGFunction/main.cpp
+++ C++ From Beginer to Beyond/Exception Handeling/MPGFunction/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+double calculate_mpg(int miles, int gallons){
+    if (gallons == 0)
+        throw 0;
+    return static_cast<double>(miles)/gallons;
+}
+
 int main() {
+    int miles{};
+    int gallons{};
+    int miles_per_gallon{};
+
+    std::cout<<"Enter the miles: ";
+    std::cin>>miles;
+    std::cout<<"Enter the gallons: ";
+    std::cin>>gallons;
+
+    try{
+        miles_per_gallon = calculate_mpg(miles, gallons);
+        std::cout<<"Result: " <<miles_per_gallon;
+    }
 
     return 0;
 }
