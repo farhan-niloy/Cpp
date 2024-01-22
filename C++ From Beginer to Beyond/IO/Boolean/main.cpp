dbfe51a12a37cefeeb54f2a73a539b3ca9dd251d
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-22 08:20:26 -0500

    update

--- C++ From Beginer to Beyond/IO/Boolean/main.cpp
+++ C++ From Beginer to Beyond/IO/Boolean/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <iomanip>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout<<"noboolalpha - default (10==10):" <<(10==10) <<std::endl;
+    std::cout<<"noboolalpha - default (10==20):" <<(10==20) <<std::endl;
+
+    std::cout<<std::boolalpha;
+    std::cout<<"noboolalpha - default (10==10):" <<(10==10) <<std::endl;
+    std::cout<<"noboolalpha - default (10==20):" <<(10==20) <<std::endl;
+    std::cout<<"noboolalpha - default (10==10):" <<(10==10) <<std::endl;
+    std::cout<<"noboolalpha - default (10==20):" <<(10==20) <<std::endl;
+
+    std::cout<<std::noboolalpha;
+    std::cout<<"noboolalpha - default (10==10):" <<(10==10) <<std::endl;
+    std::cout<<"noboolalpha - default (10==20):" <<(10==20) <<std::endl;
+
+    std::cout.setf(std::ios::boolalpha);
+    std::cout<<"noboolalpha - default (10==10):" <<(10==10) <<std::endl;
+    std::cout<<"noboolalpha - default (10==20):" <<(10==20) <<std::endl;
+
+
+
+
     return 0;
 }
