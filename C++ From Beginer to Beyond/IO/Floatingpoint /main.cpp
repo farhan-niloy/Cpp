c75ea1fe4c9ff3b1613fe1374ff1a1a7938597ac
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-22 19:57:52 +0600

    update

---
+++ C++ From Beginer to Beyond/IO/Floatingpoint /main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+
+int main() {
+    double num1 {12345689.987654321};
+    double num2 {1234.5678};
+    double num3 {1234.0};
+
+    std::cout<<"--Defaults----------------------------" <<std::endl;
+    std::cout<<num1 <<std::endl;
+    std::cout<<num2 <<std::endl;
+    std::cout<<num3 <<std::endl;
+
+    return 0;
+}
