da241eb440bd93aeed26cb3bc7d1d1a542340745
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-22 20:16:38 +0600

    update

--- C++ From Beginer to Beyond/IO/Align and Fill/main.cpp
+++ C++ From Beginer to Beyond/IO/Align and Fill/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <iomanip>
+
+void ruller(){
+    std::cout<<"\n12345678901234567890123456789012345678901234567890" <<std::endl;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int num1 {1234};
+    int num2 {12345678};
+    std::string hello {"Hello"};
+
+    std::cout<<"--Defaults------------------------------------------" <<std::endl;
+    ruller();
+    std::cout<<num1
+             <<num2
+             <<hello
+             <<std::endl;
+
+    std::cout<<"--Defaults One per Line------------------------------------------" <<std::endl;
+    ruller();
+    std::cout<<num1 <<std::endl;
+    std::cout<<num2 <<std::endl;
+    std::cout<<hello <<std::endl;
+
+    std::cout<<"--Width 10 for num1------------------------------------------" <<std::endl;
+    ruller();
+    std::cout<<std::setw(10) <<num1
+             <<num2
+             <<hello
+             <<std::endl;
+
+    std::cout<<"--Width 10 for num1------------------------------------------" <<std::endl;
+    ruller();
+    std::cout<<std::setw(10) <<num1
+             <<std::setw(10) <<num2
+             <<std::setw(10) <<hello
+             <<std::endl;
+
     return 0;
 }
