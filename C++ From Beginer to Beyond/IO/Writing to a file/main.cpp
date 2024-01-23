10bc5dbb6680e3b5e4d840066b49aaf2216d81fd
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-23 15:02:07 +0600

    update

--- C++ From Beginer to Beyond/IO/Writing to a file/main.cpp
+++ C++ From Beginer to Beyond/IO/Writing to a file/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ofstream out_file("../output.txt");
+    if(!out_file){
+        std::cerr<<"Error creating file" <<std::endl;
+        return 1;
+    }
+    std::string line;
+    std::cout<<"Enter something to write to the file: ";
+    std::getline(std::cin, line);
+    out_file <<line <<std::endl;
+
+    out_file.close();
+
     return 0;
 }
