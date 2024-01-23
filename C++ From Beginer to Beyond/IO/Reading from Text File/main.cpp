c3ea00cca3bfde7ba49815627292e975a07ff298
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-23 14:10:31 +0600

    update

--- C++ From Beginer to Beyond/IO/Reading from Text File/main.cpp
+++ C++ From Beginer to Beyond/IO/Reading from Text File/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ifstream in_file;
+    in_file.open("test.txt");
+
+    if(!in_file){
+        std::cerr<<"Problem opening file" <<std::endl;
+        return 1;
+    }
+
+    std::cout<<"File is good to go" <<std::endl;
+
     return 0;
 }
