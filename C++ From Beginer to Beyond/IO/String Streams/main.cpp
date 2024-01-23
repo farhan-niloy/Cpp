cabca3c5f7bf2959a59cc6e7df2f07b9bac32e5d
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-23 15:28:07 +0600

    update

--- C++ From Beginer to Beyond/IO/String Streams/main.cpp
+++ C++ From Beginer to Beyond/IO/String Streams/main.cpp
@@ -13,9 +13,16 @@ int main() {
     std::istringstream iss{info};
 
     iss >>name >>num >>total;
-    std::cout <<std::setw(10) <<name
+    std::cout <<std::setw(10) <<std::left <<name
               <<std::setw(5) <<num
               <<std::setw(5) <<total <<std::endl;
+
+    std::cout<<"\n---------------------------------------------------------------" <<std::endl;
+    std::ostringstream os {};
+    os <<std::setw(10) <<std::left <<name
+                    <<std::setw(5) <<num
+                    <<std::setw(5) <<total <<std::endl;
+    std::cout<<os.str() <<std::endl;
 
     return 0;
 }
