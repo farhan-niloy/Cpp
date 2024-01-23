77b5c5e2c4e4bc1e8b27c42b27074c782ca924aa
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-23 15:24:50 +0600

    update

--- C++ From Beginer to Beyond/IO/String Streams/main.cpp
+++ C++ From Beginer to Beyond/IO/String Streams/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <limits>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int num{};
+    double total{};
+    std::string name{};
+
+    std::string info {"Larry 100 1234.5"};
+    std::istringstream iss{info};
+
+    iss >>name >>num >>total;
+    std::cout <<std::setw(10) <<name
+              <<std::setw(5) <<num
+              <<std::setw(5) <<total <<std::endl;
+
     return 0;
 }
