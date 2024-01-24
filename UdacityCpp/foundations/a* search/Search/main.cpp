e7d0ba8b2136ee7113aed01f8b06f540bbcd8d2e
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-24 19:30:22 +0600

    update

--- UdacityCpp/foundations/a* search/Search/main.cpp
+++ UdacityCpp/foundations/a* search/Search/main.cpp
@@ -26,3 +26,47 @@ vector<State> ParseLine(string line){
     }
     return row;
 }
+
+vector<vector<State>> ReadBoardFile(string path) {
+    ifstream myfile(path);
+    vector<vector<State>> board{};
+    if (myfile) {
+        string line;
+        while (getline(myfile, line)) {
+            vector<State> row = ParseLine(line);
+            board.push_back(row);
+        }
+    }
+}
+
+vector<vector<State>> Search(vector<vector<State>>board, int start[], int goal[]){
+    cout<<"No path found" <<std::endl;
+    return std::vector<vector<State>>{};
+}
+
+string CellString(State cell){
+    switch(cell){
+        case State::kObstacle: return  "⛰   ";
+        default: return "0   ";
+    }
+}
+
+void PrintBoard(const vector<vector<State>> board){
+    for (int i = 0; i < board.size(); i++){
+        for (int j = 0; j < board[i].size(); j++){
+            cout<<CellString(board[i][j]);
+        }
+        cout<<"\n";
+    }
+}
+
+int main(){
+//    int init[2] = {0, 0};
+//    int goal[2] = {4, 5};
+    auto board = ReadBoardFile("../1.board");
+    vector<vector<State>> solution;
+    PrintBoard(solution);
+}
+
+
+
