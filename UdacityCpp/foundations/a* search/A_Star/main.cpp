4d376a332e38683c1d6824bbe561ff9ccb8b9191
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-24 20:38:11 +0600

    update

--- UdacityCpp/foundations/a* search/A_Star/main.cpp
+++ UdacityCpp/foundations/a* search/A_Star/main.cpp
@@ -12,7 +12,25 @@ using std::string;
 using std::vector;
 using std::abs;
 
+// TODO: Add kStart and KFinish enumerators to the State enum.
 enum class State {kEmpty, kObstacle, kClosed, kPath, kStart, kFinish};
+
+// directional deltas
+const int delta[4][2]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+vector<vector<State> ParseLine(string line){
+    istringstream sline(line);
+    int n;
+    char c;
+    vector<State> row;
+    while(sline >>n >>c && c == ','){
+        if (n == 0){
+            row.push_back(State::kEmpty);
+        }else{
+            row.push_back(State::kObstacle);
+        }
+    }
+}
 
 int main() {
 
