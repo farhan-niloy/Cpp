3a50ef32a688dca68d74ce30b1b38619ac61c343
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-24 20:53:47 +0600

    update

--- UdacityCpp/foundations/a* search/A_Star/main.cpp
+++ UdacityCpp/foundations/a* search/A_Star/main.cpp
@@ -18,19 +18,39 @@ enum class State {kEmpty, kObstacle, kClosed, kPath, kStart, kFinish};
 // directional deltas
 const int delta[4][2]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
-vector<vector<State> ParseLine(string line){
+vector<State> ParseLine(string line) {
     istringstream sline(line);
     int n;
     char c;
     vector<State> row;
-    while(sline >>n >>c && c == ','){
-        if (n == 0){
+    while (sline >> n >> c && c == ',') {
+        if (n == 0) {
             row.push_back(State::kEmpty);
-        }else{
+        } else {
             row.push_back(State::kObstacle);
         }
     }
+    return row;
 }
+
+vector<vector<State>> ReadBoardFile(string path) {
+    ifstream file (path);
+    vector<vector<State>> board{};
+    if (file) {
+        string line;
+        while (getline(file, line)) {
+            vector<State> row = ParseLine(line);
+            board.push_back(row);
+        }
+    }
+    return board;
+}
+
+/**
+ * Compare the F values of two cells.
+ */
+
+
 
 int main() {
 
