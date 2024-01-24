a5cdc20e88ac57bda520330c5432a6353d4d414c
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-24 20:31:38 +0600

    update

--- UdacityCpp/foundations/a* search/A_Star/main.cpp
+++ UdacityCpp/foundations/a* search/A_Star/main.cpp
@@ -1,6 +1,20 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::ifstream;
+using std::istringstream;
+using std::sort;
+using std::string;
+using std::vector;
+using std::abs;
+
+enum class State {kEmpty, kObstacle, kClosed, kPath, kStart, kFinish};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
