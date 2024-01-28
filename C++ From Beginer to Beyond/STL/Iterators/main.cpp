73a045c142e9a94eed840d350a49c79a7a7aa9fd
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-28 16:43:56 +0600

    update

--- C++ From Beginer to Beyond/STL/Iterators/main.cpp
+++ C++ From Beginer to Beyond/STL/Iterators/main.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <list>
+
+void display(const std::vector<int> &vec){
+    std::cout<<"[ ";
+    for(auto &i:vec){
+        std::cout <<i <<' ';
+    }
+    std::cout<< "] " <<std::endl;
+}
+
+ void test1() {
+    std::cout<<"\n============================================\n";
+    std::vector<int> num1 {1,2,3,4,5};
+    auto it = num1.begin();
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
