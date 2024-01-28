53e9636d7671dd9e66eee25324707096f79ea846
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-28 14:27:11 +0600

    update

--- C++ From Beginer to Beyond/STL/Class Template/main.cpp
+++ C++ From Beginer to Beyond/STL/Class Template/main.cpp
@@ -13,7 +13,7 @@ public:
     Item(std::string name, T value)
             : name(name), value(value) {}
 
-    const std::string& get_name() const { return name; }
+    const std::string get_name() const { return name; }
     T get_value() const { return value; }
 };
 
@@ -24,6 +24,17 @@ struct My_Pair {
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Item<int> item1{"Frank", 100};
+    std::cout<<item1.get_name() <<" " <<item1.get_value() <<std::endl;
+
+    Item <std::string> item2 {"Frank", "Professor"};
+    std::cout<<item2.get_name() <<" " <<item2.get_value() <<std::endl;
+
+    Item <Item<std::string>> item3 {"Frank", {"C++", "Professor"}};
+    std::cout <<item3.get_name() <<" "
+              <<item3.get_value().get_name()
+              <<item3.get_value().get_value() <<std::endl;
+
+
     return 0;
 }
