cf34cc6f220ac922d4ed2400cf70fdf59f19d02e
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-28 15:13:33 +0600

    update

--- C++ From Beginer to Beyond/STL/Array Template Clas/main.cpp
+++ C++ From Beginer to Beyond/STL/Array Template Clas/main.cpp
@@ -36,6 +36,21 @@ public:
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Array<5> nums;
+    std::cout <<"The size of nums is: " <<nums.get_size() <<std::endl;
+    std::cout <<nums <<std::endl;
+
+    nums.fill(0);
+    std::cout<<"The size of nums is: " <<nums.get_size() <<std::endl;
+
+    nums.fill(10);
+    std::cout<<nums <<std::endl;
+
+    nums[0] = 1000;
+    nums[3] = 2000;
+    std::cout <<nums <<std::endl;
+
+    Array<100> nums2 {1};
+    std::cout<<"This size of nums2 is: " <<nums2.get_size() <<std::endl;
+    std::cout <<nums2 <<std::endl;
 }
