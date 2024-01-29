8ccb068517f23cd12b5cf3857d298d13e3923f2e
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-01-29 14:25:31 +0600

    update

--- C++ From Beginer to Beyond/STL/Algorithmm/first.cpp
+++ C++ From Beginer to Beyond/STL/Algorithmm/first.cpp
@@ -88,13 +88,22 @@ void all_of_test(){
         std::cout<<"Not all the elements are < 20" <<std::endl;
 }
 
+void string_transform_test(){
+    std::cout<<"\n=======================================" <<std::endl;
+
+    std::string str1 {"This is a test"};
+    std::cout <<"Before transform: " <<str1 <<std::endl;
+    std::transform(str1.begin(), str1.end(), str1.begin(), ::toupper);
+    std::cout<<"After transform: " <<str1 <<std::endl;
+}
+
 int main(){
 //find_test();
 //count_test();
 //count_if_test();
 //replace_test();
-all_of_test();
-
+//all_of_test();
+string_transform_test();
 
     return 0;
 }
