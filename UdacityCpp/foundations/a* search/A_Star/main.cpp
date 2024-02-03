7fa1fab609eb9c235a6a63f96a24b75adde5dbba
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-02-03 14:06:36 +0600

    update

--- UdacityCpp/foundations/a* search/A_Star/main.cpp
+++ UdacityCpp/foundations/a* search/A_Star/main.cpp
@@ -184,7 +184,7 @@ void PrintBoard(const vector<vector<State>> board) {
 
 int main() {
     int init[2]{0, 0};
-    int goal[2]{4, 5};
+    int goal[2]{7,5};
     auto board = ReadBoardFile("../1.board");
     auto solution = Search(board, init, goal);
     PrintBoard(solution);
