872f3b3740d8aa4c0d8adb3c32d497da13ed1c80
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-02-09 23:49:19 +0600

    update

--- GameDev/Game Programming Fundamentals/Moving Circle/main.cpp
+++ GameDev/Game Programming Fundamentals/Moving Circle/main.cpp
@@ -1,20 +1,46 @@
 #include "../include/raylib.h"
 
 int main() {
-  int width{768};
-  int height{1280};
-  Color darkGreen = {0, 100, 0, 255};
+    // Window Dimension
+    int width{1400};
+    int height{700};
+    Color darkGreen = {0, 100, 0, 255};
+
+    // Circle Coordinates
+    int circle_x = static_cast<int>(width / 2);
+    int circle_y = static_cast<int>(height / 2);
+
+    InitWindow(width, height, "Farhan's Window");
+
+    SetTargetFPS(60);
+    while (WindowShouldClose() == false) {
+        BeginDrawing();
+        ClearBackground(darkGreen);
+
+        // Game Logic Begins
+        DrawCircle(circle_x, circle_y, 100, RED);
+
+        if (IsKeyDown(KEY_D)) {
+            circle_x = circle_x + 10;
+        } 
+
+        if (IsKeyDown(KEY_A)) {
+            circle_x = circle_x - 10;
+        } 
+         
+        if (IsKeyDown(KEY_W)) {
+            circle_y  -= 10;
+        } 
+
+        if (IsKeyDown(KEY_S)) {
+            circle_y += 10;
+        } 
+
+        // Game Logic Ends
+
+        EndDrawing();
+    }
 
 
-  InitWindow(width, height, "Farhan's Window");
-
-  while (WindowShouldClose() == false) {
-    BeginDrawing();
-    ClearBackground(darkGreen);
-    DrawCircle(static_cast<int>(width/2), static_cast<int>(height/2), 100, RED);
-    EndDrawing();
-  }
-
+    return 0;
 }
-
-
