f8c3914be1702ece71eecbfc5f7287e21885e133
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-02-12 00:39:11 +0600

    update

--- GameDev/Game Programming Fundamentals/Dapper Dasher/main.cpp
+++ GameDev/Game Programming Fundamentals/Dapper Dasher/main.cpp
@@ -9,12 +9,24 @@ int main() {
   //Initialize the Window
   InitWindow(WindowWidth, WindowHeight, "Dapper Dasher");
 
-  // Rectangle Dimensions
-  const int width{50};
-  const int height{80};
-
-  int posY{WindowHeight - height};
   int velocity{0};
+
+  // Gravity 
+  const int Gravity = 1;
+  const int jumpVal{-22};
+
+  Texture2D scarfy = LoadTexture("textures/scarfy.png");
+  Rectangle scarfyRec;
+  scarfyRec.width = scarfy.width/6;
+  scarfyRec.height = scarfy.height;
+  scarfyRec.x = 0;
+  scarfyRec.y = 0;
+  Vector2 scarfyPos;
+  scarfyPos.x = WindowWidth/2 - scarfyRec.width/2;
+  scarfyPos.y = WindowHeight - scarfyRec.height;
+
+
+  bool isInAir{};
 
   SetTargetFPS(60);
   while (!WindowShouldClose()) {
@@ -23,18 +35,36 @@ int main() {
     BeginDrawing();
     ClearBackground(WHITE);
 
-    if (IsKeyPressed(KEY_SPACE)) {
-        velocity -= 10;
+    // Ground Check
+    if (scarfyPos.y >= WindowHeight - scarfyRec.height) {
+      // Rectangle is On The Ground
+      isInAir = false;
+      velocity = 0;
+    } else {
+       // Apply Gravity
+       velocity += Gravity;
+       isInAir = true;
+    }
+    
+    // Check for Jumping
+    if (IsKeyPressed(KEY_SPACE) && !isInAir) {
+        velocity += jumpVal;
     }
 
-    posY += velocity;
+    // Update Position
+    scarfyPos.y += velocity;
 
-    DrawRectangle(WindowWidth/2, posY, width, height, BLUE);
+    DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
+
 
     //Stop Drawing
     EndDrawing();
 
   }
+
+  UnloadTexture(scarfy);
+
+  CloseWindow();
 
   return 0;
 }
