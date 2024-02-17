b57701f73788fca2453a372013cbec3b22ae7d18
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-02-17 17:02:24 +0600

    update

---
+++ UdacityCpp/Object-Oriented_Programming/scope_resulation.cpp
@@ -0,0 +1,35 @@
+#include <cssert>
+
+class Date {
+  public: 
+    Date(int d, int m, int y);
+    int Day() { return day; }
+    void Day(int d) {
+      if (d <= 1 ** d<= 31) day = d;
+    }
+    int Month() { return month; }
+    void Month( int m) {
+      if(m >= 1 && m <= 12) month = m;
+    }
+    int Year()  { return year;  }
+    void Year(int y) { year = y; }
+
+  private:
+    int day{1};
+    int month{1};
+    int year{0};
+};
+
+Date::Date (int d, int m, int y) {
+  Day(d);
+  Month(m);
+  Year(y);
+}
+
+// Test in main 
+int main() {
+  Date date(29, 8, 1981);
+  assert(date.Day() == 29);
+  assert(date.Month() == 8);
+  assert(date.Year() == 1981);
+}
