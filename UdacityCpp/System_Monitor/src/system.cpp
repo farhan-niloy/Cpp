4ef6b0220b29f1b8c94b343df86faae8695cdfc9
NULL <null@null-void.NULL>
2024-02-23 03:50:54 -0500

    update

--- UdacityCpp/System_Monitor/src/system.cpp
+++ UdacityCpp/System_Monitor/src/system.cpp
@@ -6,6 +6,7 @@
 
 #include "process.h"
 #include "processor.h"
+#include "linux_parser.h"
 #include "system.h"
 
 using std::set;
@@ -17,22 +18,33 @@ using std::vector;
 Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+vector<Process>& System::Processes() {
+  vector<int> pids = LinuxParser::Pids();
+  processes_.clear();
+  for (long unsigned int i = 0; i < pids.size(); i++) {
+    if(!LinuxParser::Ram(pids[i]).empty()) {
+      Process process(pids[i]);
+      processes_.push_back(process);
+    }
+  }
+  std::sort(processes_.rbegin(), processes_.rend());
+  return processes_;
+}
 
 // TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+std::string System::Kernel() { return LinuxParser::Kernel(); }
 
 // TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
+float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
 
 // TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
 
 // TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+long int System::UpTime() { return LinuxParser::UpTime();}
