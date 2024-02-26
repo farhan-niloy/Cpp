e4bd2cededbd75bbbefc9077f66c5afef02119ef
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-02-27 01:42:13 +0600

    sum of n natural number

---
+++ Data Structure & Algorithm/Recursion/Sum Of Natural Numbers/Sum Of Natural Numbers/main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  Sum Of Natural Numbers
+//
+//  Created by Niloy Farhan on 27/2/24.
+//
+
+#include <iostream>
+
+int sum3(int n);
+
+int sum2(int n);
+
+int sum(int n) {
+    if (n == 0 )
+        return 0;
+    return sum(n-1)+n;
+}
+
+int main(int argc, const char * argv[]) {
+    
+//    std::cout<<sum(3) <<" ";
+//    std::cout<<sum2(3) <<" ";
+    std::cout<<sum3(3) <<" ";
+    
+    return 0;
+}
+
+//Order of 1 O(1) complexity
+int sum2(int n) {
+    return n*(n+1)/2;
+}
+
+//Order of n O(n) complexity
+int sum3(int n) {
+    int s{0};
+    for(int i = 1; i<=n; i++)
+        s+=i;
+    return s;
+}
