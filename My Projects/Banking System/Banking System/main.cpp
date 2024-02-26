964712cfedbe00b0a1ab0f407a0f4aba7823b527
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-02-26 12:46:34 +0600

    while loop aded

--- My Projects/Banking System/Banking System/main.cpp
+++ My Projects/Banking System/Banking System/main.cpp
@@ -14,20 +14,25 @@ using std::endl;
 
 int main(int argc, const char * argv[]) {
     
-    unsigned int options;
+    bool quit = false;
     
-    cout<< "\n\n\n       SELECT ONE OPTION BELOW" <<endl;
-    cout<<"       1 Open an Account \n";
-    cout<<"       2 Balance Enquiry \n";
-    cout<<"       3 Deposit \n";
-    cout<<"       4 Withdraw \n";
-    cout<<"       5 Close an Account \n";
-    cout<<"       6 Show All Accounts \n";
-    cout<<"       7 Quit \n";
-    cout<<"\nEnter Your Choice: " ;
-    std::cin>>options;
-    cout<<options <<endl;
     
+    while(!quit) {
+        
+        unsigned int options;
+        
+        cout<< "\n\n\n       SELECT ONE OPTION BELOW" <<endl;
+        cout<<"       1 Open an Account \n";
+        cout<<"       2 Balance Enquiry \n";
+        cout<<"       3 Deposit \n";
+        cout<<"       4 Withdraw \n";
+        cout<<"       5 Close an Account \n";
+        cout<<"       6 Show All Accounts \n";
+        cout<<"       7 Quit \n";
+        cout<<"\nEnter Your Choice: " ;
+        std::cin>>options;
+        cout<<options <<endl;
+    }
     
     return 0;
 }
