737b56fe19b8f814234687b463e43cfeeba0a0ca
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-02-26 12:43:35 +0600

    done making output

---
+++ My Projects/Banking System/Banking System/main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  Banking System
+//
+//  Created by Niloy Farhan on 26/2/24.
+//
+
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+int main(int argc, const char * argv[]) {
+    
+    unsigned int options;
+    
+    cout<< "\n\n\n       SELECT ONE OPTION BELOW" <<endl;
+    cout<<"       1 Open an Account \n";
+    cout<<"       2 Balance Enquiry \n";
+    cout<<"       3 Deposit \n";
+    cout<<"       4 Withdraw \n";
+    cout<<"       5 Close an Account \n";
+    cout<<"       6 Show All Accounts \n";
+    cout<<"       7 Quit \n";
+    cout<<"\nEnter Your Choice: " ;
+    std::cin>>options;
+    cout<<options <<endl;
+    
+    
+    return 0;
+}
