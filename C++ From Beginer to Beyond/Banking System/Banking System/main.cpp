8c63b295342b1385ba1433bd3bc05f3927f96755
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-02-27 15:47:41 +0600

    update

--- C++ From Beginer to Beyond/Banking System/Banking System/main.cpp
+++ C++ From Beginer to Beyond/Banking System/Banking System/main.cpp
@@ -10,6 +10,7 @@
 
 using std::string;
 using std::cout;
+using std::cin;
 using std::endl;
 using std::ofstream;
 using std::ifstream;
@@ -61,7 +62,82 @@ public:
 };
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Bank b; 
+    Account acc;
+    
+    int choice;
+    string fname,lname;
+    
+    long accountNumber;
+    float balance;
+    float amount;
+    
+    cout<<"***Banking System***" <<endl;
+    do {
+        cout<<"\n\tSelect One Option Below ";
+        cout<<"\n\t1 Open an Account";
+        cout<<"\n\t2 Balance Enquiry";
+        cout<<"\n\t3 Deposit";
+        cout<<"\n\t4 Withdrawal";
+        cout<<"\n\t5 Close an Account";
+        cout<<"\n\t6 Show All accounts";
+        cout<<"\n\t7 Quit";
+        cout<<"\nEnter your choise: ";
+        cin>>choice;
+        
+        switch(choice) {
+            case 1:
+                cout<<"Enter First Name: ";
+                cin>>fname;
+                cout<<"Enter Last Name: ";
+                cin>>lname;
+                cout<<"Enter initial Balance: ";
+                cin>>balance;
+                acc = b.openAccount(fname, lname, balance);
+                cout<<endl <<"Congratulations Account is Created! " <<endl;
+                cout<<acc;
+                break;
+            case 2:
+                cout<<"Enter Account Number: ";
+                cin>>accountNumber;
+                acc=b.BalanceEnquiry(accountNumber);
+                cout<<endl<<"Your Account Details" <<endl;
+                cout<<acc;
+                break;
+            case 3:
+                cout<<"Enter Account Number: ";
+                cin>>accountNumber;
+                cout<<"Enter Balance: ";
+                cin>>amount;
+                acc=b.Deposit(accountNumber, amount);
+                cout<<endl <<"Amount is Deposited" <<endl;
+                cout<<acc;
+                break;
+            case 4:
+                cout<<"Enter Account Number: ";
+                cin>>accountNumber;
+                cout<<"Enter Balance: ";
+                cin>>amount;
+                acc=b.Withdraw(accountNumber, amount);
+            case 5:
+                cout<<"Enter Account Number: ";
+                cin>>accountNumber;
+                b.ColoseAccount(accountNumber);
+                cout<<endl<<"Account is Closed! " <<endl;
+                cout<<acc;
+            case 6:
+                b.ShowAllAccounts();
+                break;
+            case 7:
+                break;
+            default:
+                cout<<"\nEnter correct Choice!";
+                exit(0);
+        }
+        
+    } while(choice != 7);
+
     return 0;
 }
+
+
