2f2b5e81a0688f0873553c3e5402393065a967d6
niloy-farhan <farhansarkerniloy@gmail.com>
2024-02-29 14:04:40 +0600

    update

--- UdacityCpp/Concurrency/Intro To Concurrency/thread1.cpp
+++ UdacityCpp/Concurrency/Intro To Concurrency/thread1.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <thread>
 
-void test(int x) {
-    std::cout <<"Hello from thread!" <<std::endl;
-    std::cout <<"Argument passed in: " <<x <<std::endl;
-}
+//void test(int x) {
+//    std::cout <<"Hello from thread!" <<std::endl;
+//    std::cout <<"Argument passed in: " <<x <<std::endl;
+//}
 
 int main() {
 
-    std::thread myThread(&test, 1000);
+    //With Lamda
+    auto lamda=[](int x){
+        std::cout<<"Hello from thread" <<std::endl;
+        std::cout<<"Argument passed in: " <<x <<std::endl;
+    };
+
+    std::thread myThread(lamda, 1000);
     myThread.join();
 
     std::cout<<"Hello from my main thread" <<std::endl;
