b7d44e479d7f4de0e8cb5efddb726d6e91e6a3a1
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-02-29 18:26:55 +0600

    update

--- Modern C++ Concurrency in Depth/launching_a_thread.cpp
+++ Modern C++ Concurrency in Depth/launching_a_thread.cpp
@@ -8,7 +8,7 @@ void foo() {
 class Callable_Class {
 public:
   void operator()() {
-    printf("Hello from class with function call operator - %d \n", std::this_theread::get_id());
+    printf("Hello from class with function call operator - %d \n", std::this_thread::get_id());
   }
 };
 
@@ -28,3 +28,9 @@ void run() {
   printf("Hello from main - %d \n", std::this_thread::get_id());
 } 
 
+int main() {
+
+  run();
+
+  return 0;
+}
