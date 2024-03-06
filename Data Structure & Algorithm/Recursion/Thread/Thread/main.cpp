01b15913be835cbbbf553012216b7dc9a200cd13
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-03-06 17:25:55 +0600

    update

---
+++ Data Structure & Algorithm/Recursion/Thread/Thread/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Thread
+//
+//  Created by Niloy Farhan on 6/3/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Modern C++ Concurrency in Depth/thread.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+typedef unsigned long long ull;
+
+ull OddSum = 0;
+ull EvenSum = 0;
+
+void findEven(ull start, ull end) {
+    for (ull i = start; i <= end; i++)
+        if ((i&1) == 0)
+            EvenSum += 1;
+}
+
+void findOdd(ull start, ull end) {
+    for (ull i = start; i <= end; i++)
+        if ((i&1) != 0) {
+            OddSum += 1;
+        }
+}
+
+int main() {
+
+    ull start = 0, end = 1900000000;
+    
+    auto startTime = high_resolution_clock::now();
+
+    findOdd(start, end);
+    findEven(start, end);
+
+    auto endTime = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(endTime - startTime);
+
+    cout << "OddSum :" << OddSum << endl;
+    cout << "EvenSum :" << EvenSum << endl;
+    cout<< duration.count()/1000000 <<endl;
+
+    return 0;
+}
+
